18145b26c15de1cedb60f9255c1ba8ae2abe7922
cesarleyton <cesarleyton549@gmail.com>
2025-10-25 17:25:30 -0500

    add exit command

--- app/main.ts
+++ app/main.ts
@@ -7,8 +7,19 @@ const rl = createInterface({
 
 function loop() {
   rl.question("$ ", (answer) => {
+    //exit utility shall cause the shell to exit from its current execution env with
+    // the exit status specified by the unsigned decimal integer n which can be represented
+    // using 1 byte
+    // .
+    if (answer === "exit 0") {
+      rl.write("", {
+        ctrl: true,
+        name: "c",
+      });
+      return;
+    }
+
     rl.write(answer + ":" + " command not found\n");
-    // rl.close();
     return loop();
   });
 }
