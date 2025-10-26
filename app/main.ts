ed302ce0dedec36c1e1537df4bce909080f046fe
cesarleyton <cesarleyton549@gmail.com>
2025-10-26 08:36:28 -0500

    add echo command

--- app/main.ts
+++ app/main.ts
@@ -5,12 +5,32 @@ const rl = createInterface({
   output: process.stdout,
 });
 
+type ParsedCommand = {
+  command: string;
+  args: string;
+};
+
+function parseCommand(input: string): ParsedCommand {
+  // we should remove whitespaces on both ends but respect casing
+  const normalizedCommand = input.trim();
+  const [command, ...args] = normalizedCommand.split(" ");
+
+  return {
+    command,
+    args: args.join(" "),
+  };
+}
 function loop() {
   rl.question("$ ", (answer) => {
+    const { command, args } = parseCommand(answer);
+
+    if (command === "echo") {
+      console.log(`${args}`);
+      return loop();
+    }
     //exit utility shall cause the shell to exit from its current execution env with
     // the exit status specified by the unsigned decimal integer n which can be represented
     // using 1 byte
-    // .
     if (answer === "exit 0") {
       rl.write("", {
         ctrl: true,
